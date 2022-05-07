cac73fee295c16cbb2f5ed09add621467c1ad92b
ledford30 <101572791+Vesuro30@users.noreply.github.com>
2022-05-07 09:25:30 -0600

    add comments

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+// This is my take on a random password generator.  
+//
+// Below we will initialize arrays which we will use for picking the random characters from
+// We are also initializing the numChar and numCharValue to account for the slider
+// we are using that allows users to select the length of their password
+
+
 var lettersLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var lettersUppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
@@ -10,7 +17,7 @@ numChar.addEventListener("input", function(){
   numCharValue.innerHTML = numChar.value;
 });
 
-// Assignment Code
+// Assignment Code - this selects the generate button, or more specifically which element has the generate ID
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -21,9 +28,14 @@ function writePassword() {
 };
 
 
-// function randomLowerLetter(){
-// var randomLower = letters[Math.floor(Math.random()*letters.length)];
-// };
+// The function below generatePassword is used to verify that we have all types selected by the user
+// useLower, useUpper, useNum and useSpec are what specifically we are using to verify if the user 
+// has checked the check boxes that allow the user to select what types of characters they would like the
+// password generator to use.  Then we are using haveLower, haveUpper, haveNum and haveSpec to verify if we have
+// the selected type of character.  These are all initialized as false to begin with and we turn them to 
+// true once the selected character type has been added.  We then use the function generateRandNum to select a
+// random index from the specified array.
+
 
 
 function generatePassword(){
@@ -36,6 +48,9 @@ var pw = "";
 var i;
 var haveLower = false, haveUpper = false, haveNum = false, haveSpec = false;
 
+function generateRandNum(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
 
 
   if (!useLower && !useUpper && !useNum && !useSpec) {
@@ -62,22 +77,48 @@ var haveLower = false, haveUpper = false, haveNum = false, haveSpec = false;
         haveSpec = true;
       };
 
-      
+
+
+      // Can also use ASCII code to generate the characters with the exception of the special chars as the codes for special chars have gaps
+      // This method also functions and may be less resource intensive?  I ultimately decided to go with the randomly generated index of the 
+      // arrays initialized at the top.
+
+      // if(useLower && !haveLower){
+      //   pw += String.fromCharCode(generateRandNum(97, 122));
+      //   haveLower = true;
+      // }
+      // if(useUpper && !haveUpper){
+      //   pw += String.fromCharCode(generateRandNum(65, 90));
+      //   haveUpper = true;
+      // }
+      // if(useNum && !haveNum){
+      //   pw =+ String.fromCharCode(generateRandNum(48, 57));
+      //   haveNum = true;
+      // }
+
+      // if(useSpec && !haveSpec){
+      //   pw += special[generateRandNum(0, 31)];
+      //   haveSpec = true;
+      // }
+
+      // Played with switch control structure.
+
       // switch(true){
         //   case charType == 0 && useLower :
-        //     pw += lettersLower[generateRandNum(0, 25)];
-        
-        //   break;
-        
+        //     pw += lettersLower[generateRandNum(0, 25)];      
+        //   break;       
         //   case charType == 1 && useUpper :
-        //     pw += lettersUppercase[generateRandNum(0, 25)];
-        
-        
+        //     pw += lettersUppercase[generateRandNum(0, 25)]
         //   break;
-        
         // }
         
         
+// Here we use generateRandNum again to randomly select which type of character (assuming the user selected more than one type)
+// is assigned to the password (pw).  We verify that the user has selected that type of character through the use of the 
+// useLower, useUpper, useNum and useSpec in our logic below.  So if the user has selected that type of character AND the 
+// current length of pw is less than the selected length (numChar.value) we add another randomly generated type and character.
+// Finally we return the pw to the user
+
         var charType = generateRandNum(0, 3);
 
       if(pw.length < numChar.value){
@@ -93,15 +134,12 @@ var haveLower = false, haveUpper = false, haveNum = false, haveSpec = false;
       
       
       
-       console.log(pw.length)
+      //  console.log(pw.length)
     };
   };
   return pw;  
 };
 
-function generateRandNum(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) ) + min;
-}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
